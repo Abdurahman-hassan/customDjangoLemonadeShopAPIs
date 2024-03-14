ab47f6d8a8b2b234dc47a830506fd97e6cecab05
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-03-14 17:58:47 +0200

    feat(urls.py): Update API endpoints and add new routes for user group management (#master)

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,9 +1,23 @@
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
+
+from LittleLemonAPI.views import (DeliveryUsersListCreateAPIView,
+                                  UserDetailAPIView,
+                                  ManagerViewSet)
+
+router = DefaultRouter()
+router.register(r'groups/manager/users', ManagerViewSet, basename='manager-users')
 
 urlpatterns = [
+    path('api/', include(router.urls)),
+
+    path('groups/delivery-crew/users', DeliveryUsersListCreateAPIView.as_view(), name='delivery-crew-users'),
+
+    path('groups/delivery-crew/users/<int:id>', UserDetailAPIView.as_view(), name='delivery_crew_user_detail'),
 
     path('api-token-auth', obtain_auth_token, name='api_token_auth'),
     path('__debug__/', include('debug_toolbar.urls')),
 
 ]
+urlpatterns += router.urls
