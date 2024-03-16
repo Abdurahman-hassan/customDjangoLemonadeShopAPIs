6a1c79ec75b5c7af9519dbe0b9a47f192715f042
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-03-16 04:14:37 +0200

    add CartSerializer, add OrderItemSerializer, add OrderSerializer

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from LittleLemonAPI.models import MenuItem
+from LittleLemonAPI.models import MenuItem, Cart, Order, OrderItem
 
 
 class UserMembershipSerializer(serializers.ModelSerializer):
@@ -22,3 +22,24 @@ class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = ['id', 'title', 'price', 'inventory', 'category']
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ['menu_items', 'quantity', 'unit_price', 'price']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['menu_item', 'quantity', 'unit_price', 'price']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True, read_only=True)
+    # order_id = serializers.IntegerField(source='id', read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'delivery_crew', 'status', 'total', 'date', 'order_items']
