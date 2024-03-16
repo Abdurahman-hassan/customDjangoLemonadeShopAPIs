334acbe96a25160c0294296abd0e88849fdd88e6
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-03-16 04:14:15 +0200

    add IsOrderOwnerOrStaff permission

--- LittleLemonAPI/permissions.py
+++ LittleLemonAPI/permissions.py
@@ -21,3 +21,21 @@ class IsDelivery(permissions.BasePermission):
     def has_permission(self, request, view):
         # Check if the user is in the delivery group
         return request.user.groups.filter(name='Delivery-crew').exists()
+
+
+# TODO: Add a new permission class that will allow only users who are not in the delivery group to access the endpoint.
+
+# class IsNotDelivery(permissions.BasePermission):
+#     def has_permission(self, request, view):
+#         return not request.user.groups.filter(name='Delivery-crew').exists()
+
+
+class IsOrderOwnerOrStaff(permissions.BasePermission):
+    """Custom permission to only allow owners of an order to see it, or staff under certain conditions."""
+
+    def has_object_permission(self, request, view, obj):
+        # Allow order owners to view
+        if obj.user == request.user:
+            return True
+        # Allow Managers and Delivery Crew special permissions
+        return request.user.groups.filter(name__in=['Manager', 'Delivery-crew']).exists()
