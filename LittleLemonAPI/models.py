4011e082f78541fccae0859ca4ef834f77cb18bf
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-03-16 04:13:50 +0200

    update MenuItems, add Cart Model, add Order and OrderItem models

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -12,11 +13,49 @@ class Category(models.Model):
 
 
 class MenuItem(models.Model):
-    title = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    inventory = models.IntegerField()
-    # we used on_delete=models.PROTECT to prevent deleting the category if it has a menu item
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='menu_items', default=1)
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    featured = models.BooleanField(default=False, db_index=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='menu_items')
 
     def __str__(self):
-        return self.title
+        return f"{self.id} {self.title}"
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menu_items = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ('user', 'menu_items')
+
+    def __str__(self):
+        return f"{self.user.username}'s cart"
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    delivery_crew = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='delivery_crew', null=True)
+    status = models.BooleanField(default=0, db_index=True)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateField(auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s order"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ('order', 'menu_item')
+
+    def __str__(self):
+        return f"{self.order.user.username}'s order item"
